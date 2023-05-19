47183f4674409e19aeab08b842f2ae0b092c2999
unknown <anikm2982@gmail.com>
2023-05-19 11:48:41 +0600

    gallery added

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import ToyGallery from './ToyGallery';
 
 const Home = () => {
     return (
         <div>
-            <Banner/>
+            <Banner />
+            <ToyGallery />
         </div>
     );
 };
---
+++ src/components/Pages/Home/ToyGallery.jsx
@@ -0,0 +1,39 @@
+const ToyGallery = () => {
+    return (
+        <div className='my-12 px-10'>
+            <p className='font-extrabold text-3xl text-blue-900 text-center my-10'>Toy Gallery</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="patryk-norman-BBQOQFk-OICg-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="marcos-ferreira-QYBVbw-JYx-NA-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="patryk-norman-ZR-AL5r0-KZA-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="aaron-cass-GJuh-DRb-Mj7g-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="maha-khairy-3uu-LWb6a-QXc-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="tengyart-RZfb8-FFd2g-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="branden-skeli-x-KTp-Y-Ny-Ss-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="valentino-montironi-av-Il29-hxao-unsplash" />
+  </div>
+  <div className="rounded-lg overflow-hidden">
+    <img className="w-full" src="https://i.pinimg.com/originals/a7/3e/70/a73e70896bc8cc656bfd64391a1862f0.jpg" alt="valentino-montironi-av-Il29-hxao-unsplash" />
+  </div>
+</div>
+
+        </div>
+    );
+};
+
+export default ToyGallery;
